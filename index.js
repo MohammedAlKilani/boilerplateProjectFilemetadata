b9c653cee29820c421661827608c7bae748edffa
MohammedAlKilani <120374233+MohammedAlKilani@users.noreply.github.com>
2023-08-04 16:24:03 +0000

    init

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.use(cors());
@@ -11,6 +12,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse",upload.single('upfile'),(req, res)=>{
+ let {originalname,mimetype,size} = req.file
+  res.json({"name":originalname,"type":mimetype,"size":size})
+})
 
 
 
